335075da6b37921b93e8d388a6bcc648f35ac51a
yrockfield <warusuke@gmail.com>
2019-04-08 17:37:31 +0900

    modified

--- AtCoderPractice/05-ABC087B/Main.java
+++ AtCoderPractice/05-ABC087B/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
 public class Main{
 	public static void main(String[] args) throws IOException{
