1382e603c24dfdd690537bd732c248519a712306
yrockfield <warusuke@gmail.com>
2019-04-08 16:23:25 +0900

    uploaded

---
+++ AtCoderPractice/04-ABC081B/Main.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main{
+	public static void main(String[] args) throws IOException{
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.nextLine());
+		String inpStr = sc.nextLine();
+		String nextStr = "";
+		int kaisu = 0;
+
+		while(inpStr != null) {
+			inpStr = canContinue(n, inpStr);
+			if (inpStr != null ){
+				kaisu ++;
+			}
+		}
+
+		sc.close();
+
+		// 出力
+		System.out.println(kaisu);
+	}
+
+	private static String canContinue(int n , String inp){
+
+		StringBuilder nextStr = new StringBuilder();
+
+		String[] inpArr =  inp.split(" ");
+
+		for (int i=0; i < n; i++){
+			if (Integer.parseInt(inpArr[i]) % 2 != 0){
+				return null;
+			}else{
+				nextStr.append( Integer.parseInt(inpArr[i]) / 2 );
+				nextStr.append( " ");
+			}
+		}
+
+		return nextStr.toString();
+	}
+}
