11c68ba8b3211e7a5063d47e42185d38a439fcdb
yrockfield <warusuke@gmail.com>
2019-04-09 11:50:58 +0900

    処理をシンプルにした

--- AtCoderPractice/05-ABC087B/Main.java
+++ AtCoderPractice/05-ABC087B/Main.java
@@ -14,35 +14,14 @@ public class Main{
 
 		int ptn = 0;
 
-		loop500:
 		for (int i=0; i<=coins500; i++) {
-			if(500 * i > kingaku){
-				break loop500;
-			}else if (500 * i == kingaku){
-				ptn++;
-			}else{
-				loop100:
-				for (int j=0; j<=coins100; j++ ){
-					if(500 * i + 100 * j > kingaku ){
-						break loop100;
-					}else if (500 * i + 100 * j == kingaku ){
+			for (int j=0; j<=coins100; j++ ){
+				for(int k=0; k<=coins50; k++ ){
+					if (500 * i + 100 * j + 50 * k == kingaku ){
 						ptn++;
-					}else{
-						loop50:
-						for(int k=0; k<=coins50; k++ ){
-							if(500 * i + 100 * j + 50 * k > kingaku ){
-								break loop50;
-							}else if (500 * i + 100 * j + 50 * k == kingaku ){
-								ptn++;
-							}
-						}
 					}
-
 				}
-
 			}
-
-
 		}
 		
 		// 出力
